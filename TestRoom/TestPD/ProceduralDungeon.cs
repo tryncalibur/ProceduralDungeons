0a82a374e8b09fd00b01e069cbf15cf3b0f42db1
tryncalibur <tryncalibur@gmail.com>
2020-06-29 20:02:34 -0700

    Implementing Initial dungeon setup

---
+++ TestRoom/TestPD/ProceduralDungeon.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class ProceduralDungeon {
+    private class room
+    {
+        Random rnd = new Random();
+        bool Up
+        { get; set; }
+        bool Down
+        { get; set; }
+        bool Left
+        { get; set; }
+        bool Right
+        { get; set; }
+
+        int Status
+        { get; set; }
+        private room()
+        {
+            Up = rnd.Next(2) == 1;
+            Down = rnd.Next(2) == 1;
+            Left = rnd.Next(2) == 1;
+            Right = rnd.Next(2) == 1;
+            Status = 0;
+        }
+    }
+
+
+    ProceduralDungeon(int r, int c)
+    {
+
+    }
+}
---
+++ TestRoom/TestPD/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TestPD{
+    class Program{
+        static void Main(string[] args){
+            string myString = "Hello";
+            Console.WriteLine(myString.IndexOf("lll"));
+        }
+    }
+}
