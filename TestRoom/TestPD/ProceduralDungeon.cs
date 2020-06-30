c2236e68a270af33211a76a3e3c49e828d66c2ec
tryncalibur <tryncalibur@gmail.com>
2020-06-29 20:25:59 -0700

    Inital gen and clean edges implemented

--- TestRoom/TestPD/ProceduralDungeon.cs
+++ TestRoom/TestPD/ProceduralDungeon.cs
@@ -1,21 +1,24 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 
 public class ProceduralDungeon {
+
+    // Data about each room
     private class room
     {
         Random rnd = new Random();
-        bool Up
+        public bool Up
         { get; set; }
-        bool Down
+        public bool Down
         { get; set; }
-        bool Left
+        public bool Left
         { get; set; }
-        bool Right
+        public bool Right
         { get; set; }
 
         int Status
         { get; set; }
-        private room()
+        public room()
         {
             Up = rnd.Next(2) == 1;
             Down = rnd.Next(2) == 1;
@@ -25,9 +28,29 @@ public class ProceduralDungeon {
         }
     }
 
-
-    ProceduralDungeon(int r, int c)
+    private room[,] dungeon;
+    public void Generate(int r, int c)
     {
+        // Generate all rooms
+        dungeon = new room[r, c];
+        for (int i = 0; i < r; ++i)
+        {
+            for (int j = 0; j < c; ++j)
+            {
+                dungeon[i, j] = new room();
+            }
+        }
 
+        // Clean Edges
+        for (int i = 0; i < r; ++i)
+        {
+            dungeon[i, 0].Left = false;
+            dungeon[i, c - 1].Right = false;
+        }
+        for (int i = 0; i < c; ++i)
+        {
+            dungeon[0, i].Up = false;
+            dungeon[r-1, i].Down = false;
+        }
     }
 }
